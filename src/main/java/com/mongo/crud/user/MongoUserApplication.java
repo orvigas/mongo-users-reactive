cc414ad1a58ace3126d2f8cc61c96e17b6c604ec
Orlando Villegas Galán <orvigas@gmail.com>
2021-09-10 12:11:21 -0500

    Ramed app and artifacts (#2)

--- src/main/java/com/mongo/crud/user/MongoUserApplication.java
+++ src/main/java/com/mongo/crud/user/MongoUserApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongoUsuariosApplication {
+public class MongoUserApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(MongoUsuariosApplication.class, args);
+		SpringApplication.run(MongoUserApplication.class, args);
 	}
 
 }
